d31742f04f98b9fb83c34f2213d444f544b8b7f2
Michel Oliveira <michel.s.oliveira@gmail.com>
2018-10-22 16:04:14 -0300

    imprimindo tipo de variaveis

--- in-out.py
+++ in-out.py
@@ -3,3 +3,10 @@ print("hello \npulando linha")
 
 nome = 'michel' #prenchendo variaveis
 print(nome) #printando valores de variaveis
+
+tipo = type(nome) #tipo da variavel
+print(tipo) #print tipo
+
+idade = 25 #variavel int
+tipo_idade = type(idade) #recebe o tipo da variavel
+print(tipo_idade) #imprime o tipo
