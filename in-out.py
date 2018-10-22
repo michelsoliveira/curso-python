1cf87b90af9067a865c2b9fd1133b7a07da3fd51
Michel Oliveira <michel.s.oliveira@gmail.com>
2018-10-22 16:10:48 -0300

    concatenandos objetos no print

--- in-out.py
+++ in-out.py
@@ -11,6 +11,8 @@ idade = 25 #variavel int
 tipo_idade = type(idade) #recebe o tipo da variavel
 print(tipo_idade) #imprime o tipo
 
-altura = 1.69 #tipo float usar ponto
+altura = 1.69 #tipo float, usar ponto
 tipo_altura = type(altura)
 print(tipo_altura)
+
+print(nome, 'tem', idade, 'anos') #imprimindo frases concatenadas!
